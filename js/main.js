ffcec253eed2ac6e53e25ea19b9fef31d3e8abfa
sonalisonawane <66239376+sonalisonawane@users.noreply.github.com>
2020-11-08 23:54:39 -1100

    added scrolling effect

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
+// slides fade animation
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+    showSlides((slideIndex += n));
 }
-setInterval(function () {
-  plusSlides(1);
+setInterval(function() {
+    plusSlides(1);
 }, 3 * 1000);
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("img");
+    var i;
+    var slides = document.getElementsByClassName("img");
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "flex";
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
+
+// slides fade animation end
+
+// background - color
+function scrollWin() {
+
+
+    bodyEl = document.querySelector("body")
+    if (window.pageYOffset >= 500) {
+        bodyEl.classList.add("changeColor")
+        bodyEl.style.color = "white"
+
+    }
+    if (window.pageYOffset <= 500) {
+        bodyEl.classList.remove("changeColor")
+        bodyEl.style.color = "black"
+
+    }
 }
